5faff4fee819898cb8a7c302282bd789c429a35e
yousukeabe0403 <abe@viva-next.com>
2016-08-09 19:24:55 +0900

    main_sourse_upload

---
+++ index.php
@@ -0,0 +1,201 @@
+<!doctype html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+		<meta name="keywords" content="HTML,CSS,JavaScript,特殊文字,リファレンス">
+		<meta name="description" content="HTML・CSS等の早見表的リファレンス">
+		<meta name="robots" content="noindex,nofollow">
+		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
+		<link rel="stylesheet" type="text/css" href="top.css">
+		<script>
+		var pagetop, glovalmenu, yPos;
+		function yScroll(){
+			pagetop = document.getElementById('pagetop');
+			glovalmenu = document.getElementById('glovalmenu');
+			yPos = window.pageYOffset;
+			if(yPos > 150){
+				pagetop.style.height = "120px";
+				pagetop.style.paddingTop = "50px";
+				glovalmenu.style.height = "50px";
+
+			} else {
+				pagetop.style.height = "36px";
+				pagetop.style.paddingTop = "8px";
+				glovalmenu.style.height = "0px";
+			}
+		}
+		window.addEventListener("scroll", yScroll);//スクロールしたら発火。コントロール付属のスクロールバー位置が変更されたときに発火
+		</script>
+<!--[if lt IE 9]>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<![endif]-->
+<!--[if gte IE 9]><!-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+    <script src="js/jquery-migrate-1.2.1.min.js"></script>
+
+<!--><![endif]-->
+	<script src="js/top.js"></script>
+	<?php
+	class UserAgent{
+		private $ua;
+		private $device;
+		public function set(){
+			$this->ua = mb_strtolower($_SERVER['HTTP_USER_AGENT']);
+			if(strpos($this->ua,'iphone') !== false){
+				$this->device = 'mobile';
+			}elseif(strpos($this->ua,'ipod') !== false){
+				$this->device = 'mobile';
+			}elseif((strpos($this->ua,'android') !== false) && (strpos($this->ua, 'mobile') !== false)){
+				$this->device = 'mobile';
+			}elseif((strpos($this->ua,'windows') !== false) && (strpos($this->ua, 'phone') !== false)){
+				$this->device = 'mobile';
+			}elseif((strpos($this->ua,'firefox') !== false) && (strpos($this->ua, 'mobile') !== false)){
+				$this->device = 'mobile';
+			}elseif(strpos($this->ua,'blackberry') !== false){
+				$this->device = 'mobile';
+			}elseif(strpos($this->ua,'ipad') !== false){
+				$this->device = 'tablet';
+			}elseif((strpos($this->ua,'windows') !== false) && (strpos($this->ua, 'touch') !== false && (strpos($this->ua, 'tablet pc') == false))){
+				$this->device = 'tablet';
+			}elseif((strpos($this->ua,'android') !== false) && (strpos($this->ua, 'mobile') === false)){
+				$this->device = 'tablet';
+			}elseif((strpos($this->ua,'firefox') !== false) && (strpos($this->ua, 'tablet') !== false)){
+				$this->device = 'tablet';
+			}elseif((strpos($this->ua,'kindle') !== false) || (strpos($this->ua, 'silk') !== false)){
+				$this->device = 'tablet';
+			}elseif((strpos($this->ua,'playbook') !== false)){
+				$this->device = 'tablet';
+			}else{
+				$this->device = 'others';
+			}
+			return $this->device;
+		}
+	}
+
+	$ua = new UserAgent();
+	?>
+
+	</head>
+	<body>
+<div id="loader-bg">
+	<div id="loader">
+	<div id="cssload-loader">
+		<div class="tablecell">
+			<ul>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+			</ul>
+			<p>NOW LOADING</p>
+		</div>
+	</div>
+	</div>
+</div>
+<div id="wrap">
+		<header id="pagetop">
+			<h1 id="site-logo">C plus</h1>
+			<div id="glovalmenu">
+				<ul>
+					<li>MENU1</li>
+					<li>MENU2</li>
+					<li>MENU3</li>
+					<li>MENU4</li>
+					<li>MENU5</li>
+					<li>MENU6</li>
+				</ul>
+				</div>
+		</header>
+		<div class="background-wrap">
+			<?php if($ua->set() === "mobile" || $ua->set() === "tablet"):?>
+				<div id="mobile-bg-elem">
+					<!-- <img src="video.gif"> -->
+
+				</div>
+
+
+			<?php else:?>
+				<video id="video-bg-elem" preload="auto" autoplay="true" loop="loop" muted="muted">
+					<source src="video.mp4" type="video/mp4">
+						video not supported
+				</video>
+			<?php endif;?>
+			
+			<div class="contents">
+				<div class="bg_videotext">
+					<h2 id="massage01">video background title</h2>
+					<p id="massage02">this is the tutorial video.this is the tutorial video.</p>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="detail">
+			<span class="zigzag-border"></span>
+			<div class="menu">
+
+					<h2 id="titleTxt01" style="font-size:50px; margin-bottom:30px;">video background TEST</h2>
+					<p id="titleTxt02">これはテキストです。これはテキストです。これはテキストです。これはテキストです。これはテキストです。これはテキストです。</p>
+
+					<ul>
+						<li id="titleTxt03">
+							<a href="#">
+								<div class="image-card">
+									<div class="bluebg"><div class="item"><span>ROOM A</span></div></div>
+									<div class="overlay">
+										<span></span>
+										<span></span>
+										<span></span>
+										<span></span>
+									</div>
+									<img src="image/img01.jpg" alt="">
+								</div>
+							</a>
+						</li>
+						<li id="titleTxt04">
+							<a href="#">
+								<div class="image-card">
+									<div class="bluebg"><div class="item"><span>ROOM B</span></div></div>
+									<div class="overlay">
+										<span></span>
+										<span></span>
+										<span></span>
+										<span></span>
+									</div>
+									<img src="image/img02.jpg" alt="">
+								</div>
+							</a>
+						</li>
+						<li id="titleTxt05">
+							<a href="#">
+								<div class="image-card">
+									<div class="bluebg"><div class="item"><span>ROOM C</span></div></div>
+									<div class="overlay">
+										<span></span>
+										<span></span>
+										<span></span>
+										<span></span>
+									</div>
+									<img src="image/img03.jpg" alt="">
+								</div>
+							</a>
+						</li>
+					</ul>
+			</div>
+			<!-- <span class="zigzag-border-close"></span> -->
+		</div>
+		<div class="detail">
+			<span class="zigzag-border-close"></span>
+
+			<div class="aboutus">
+				<img src="image/main_aboutous.jpg">
+
+
+			</div>
+
+		</div>
+</div>
+	</body>
+</html>
---
+++ js/top.js
@@ -0,0 +1,106 @@
+	$(function() {
+	  var h = $(window).height();
+	 
+	  $('#wrap').css('display','none');
+	  $('#loader-bg ,#loader').height(h).css('display','block');
+	});
+
+	jQuery.event.add(window,"load",function() { //全ての読み込みが完了したら実行
+	  $('#loader-bg').delay(900).fadeOut(800);
+	  $('#loader').delay(600).fadeOut(300);
+	  $('#wrap').css('display', 'block');
+	});	
+
+	$(window).load(function () {
+			$('#massage01').delay(1500).animate({
+					'opacity':'1',
+					'top':'0'
+			},{ duration: 600, easing: 'swing', });
+			$('#massage02').delay(1500).animate({
+					'opacity':'1',
+					'top':'0'
+			},{ duration: 600, easing: 'swing', });
+	});
+$(function(){
+    $(window).scroll(function () {
+        // var ScrTop = $(document).scrollTop();
+ 		var windowHeight = $(window).height();
+        var windowScrolltop = $(window).scrollTop();
+        var scrollBottom = windowHeight + windowScrolltop;
+        var offsetTop01 = $('#titleTxt01').offset().top;
+        var offsetTop02 = $('#titleTxt02').offset().top;
+        var offsetTop03 = $('#titleTxt03').offset().top;
+        var offsetTop04 = $('#titleTxt04').offset().top;
+        var offsetTop05 = $('#titleTxt05').offset().top;
+        console.log(windowScrolltop,windowHeight,scrollBottom,offsetTop01,offsetTop02);
+        if(scrollBottom-100 > offsetTop01){
+        	$('#titleTxt01').delay(200).animate({
+        		opacity:1
+        	},'slow');
+        }
+        if(scrollBottom-100 > offsetTop02){
+        	$('#titleTxt02').delay(200).animate({
+        		opacity:1
+        	},'slow');
+        }
+        if(scrollBottom-100 > offsetTop03){
+        	$('#titleTxt03').delay(200).animate({
+        		opacity:1,
+        		top:0
+        	},'slow');
+
+	        if(scrollBottom-100 > offsetTop04){
+	        	$('#titleTxt04').delay(300).animate({
+        			opacity:1,
+        			top:0
+	        	},'slow');
+	
+		        if(scrollBottom-100 > offsetTop05){
+		        	$('#titleTxt05').delay(400).animate({
+        				opacity:1,
+        				top:0
+		        	},'slow');
+		        }
+
+	        }
+        }
+
+    });
+});
+
+	// $(function() {
+ //    	var offsetTop01 = $('#titleTxt').offset().top;
+ //    	var ingFlag01 = false;
+	//     var	scrollValue = $(window).scrollTop();
+	// 	console.log(offsetTop01,scrollValue,ingFlag01);
+ //        var floatMenu = function() {
+ //        	if (scrollValue > offsetTop01+500 && ingFlag01==false) {
+	// 			ingFlag01=true;
+ //            	$('#titleTxt').animate({
+	// 				opacity:1
+ //            	},'slow');
+ //        	}
+ //    	}
+ //        $(window).scroll(floatMenu);
+ //    });
+
+
+	// $(window).load(function () { //全ての読み込みが完了したら実行
+	//   $('#loader-bg').delay(900).fadeOut(800);
+	//   $('#loader').delay(600).fadeOut(300);
+	//   $('#wrap').css('display', 'block');
+	//   $('#massage').delay(900).animate({
+	// 	'opacity':'0.5'
+	// 	},'slow');;
+	// });
+	 
+	//10秒たったら強制的にロード画面を非表示
+	$(function(){
+	  setTimeout('stopload()',10000);
+	});
+	 
+	function stopload(){
+	  $('#wrap').css('display','block');
+	  $('#loader-bg').delay(900).fadeOut(800);
+	  $('#loader').delay(600).fadeOut(300);
+	}
